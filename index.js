6fe0776c1e1b2e9605bed7c9949b3422a1a8b45a
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-12-27 19:01:30 -0800

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function userInput(){
+    return inquirer.prompt([
+  
+    {name: 'title',
+    message: 'What is the title of this application?',
+    type: 'input'},
+    {name: 'description',
+    message: 'What does this application do?',
+    type: 'input'},
+    {name: 'install',
+    message: 'How do I install this application?',
+    type: 'input'},
+    {name: 'usage',
+    message: 'How do I use this application?',
+    type: 'input'},
+    {name: 'contributing',
+    message: 'How do I contribute to this application?',
+    type: 'input'},
+    {name: 'testing',
+    message: 'How do I test this application?',
+    type: 'input'},
+    {name: 'username',
+    message: 'What is your Github username?',
+    type: 'input'},
+    {name: 'email',
+    message: 'What is your e-mail?',
+    type: 'input'},
+    {name: 'fileName',
+    message: 'What do you want to call this readme?',
+    type: 'input'},
+])};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
